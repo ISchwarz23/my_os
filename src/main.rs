a1b0bbf08e2a7c72c590579e827cfd54ae77403d
Ingo Schwarz <ingo.schwarz23@gmail.com>
2026-01-13 17:46:42 +0100

    implement println macro

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,8 @@ mod vga_buffer;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
+    println!("Hello, {}!", "World");
+    println!(":)");
 
     loop {}
 }
